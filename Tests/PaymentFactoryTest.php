f2f0472d23b8321e20d01f79ae3519b5a4ca89c8
Kotlyar Maksim <kotlyar.maksim@gmail.com>
2014-12-16 20:46:06 +0000

    [payment-factories] finish redesign.

---
+++ Tests/PaymentFactoryTest.php
@@ -0,0 +1,62 @@
+<?php
+namespace Payum\Paypal\ProCheckout\Nvp\Tests;
+
+use Payum\Paypal\ProCheckout\Nvp\PaymentFactory;
+
+class PaymentFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function shouldImplementPaymentFactoryInterface()
+    {
+        $rc = new \ReflectionClass('Payum\Paypal\ProCheckout\Nvp\PaymentFactory');
+
+        $this->assertTrue($rc->implementsInterface('Payum\Core\PaymentFactoryInterface'));
+    }
+
+    /**
+     * @test
+     */
+    public function couldBeConstructedWithoutAnyArguments()
+    {
+        new PaymentFactory();
+    }
+
+    /**
+     * @test
+     */
+    public function shouldAllowCreatePayment()
+    {
+        $factory = new PaymentFactory();
+
+        $payment = $factory->create(array(
+            'username' => 'aName',
+            'password' => 'aPass',
+            'partner' => 'aPartner',
+            'vendor' => 'aVendor',
+            'tender' => 'aTender'
+        ));
+
+        $this->assertInstanceOf('Payum\Core\Payment', $payment);
+
+        $this->assertAttributeNotEmpty('apis', $payment);
+        $this->assertAttributeNotEmpty('actions', $payment);
+
+        $extensions = $this->readAttribute($payment, 'extensions');
+        $this->assertAttributeNotEmpty('extensions', $extensions);
+    }
+
+    /**
+     * @test
+     *
+     * @expectedException \Payum\Core\Exception\LogicException
+     * @expectedExceptionMessage The username, password, partner, vendor, tender fields are required.
+     */
+    public function shouldThrowIfRequiredOptionsNotPassed()
+    {
+        $factory = new PaymentFactory();
+
+        $factory->create();
+    }
+}
